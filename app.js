38d8324f1eac31768dd5255b5b0fa9aaaadceb71
LukaisSciandra <lukaisdsciandra@gmail.com>
2026-03-02 22:28:32 -0500

    Update income and expense categories in app.js
    From Venmo to Cash

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
    ============================================ */
 
 const CATEGORIES = {
-  income:  ['Rent', 'Salary', 'Venmo', 'Other'],
-  expense: ['Rent', 'Credit Card', 'Venmo', 'Investment', 'Other'],
+  income:  ['Rent', 'Salary', 'Cash', 'Other'],
+  expense: ['Rent', 'Credit Card', 'Cash', 'Investment', 'Other'],
 };
 
 const CREDIT_CARDS = [
